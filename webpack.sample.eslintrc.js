bd0695f1af7465717a7caa5fadce4725d0661c55
Валентин Немцев <v-nemcev@yandex-team.ru>
2016-09-30 16:41:21 +0300

    Update sample config for webpack

--- webpack.sample.eslintrc.js
+++ webpack.sample.eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
     extends: 'qe-frontend',
     settings: {
         // npm install -DE eslint-import-resolver-webpack
-        'import/resolver': {
-            webpack: {
-                config: 'build/webpack.config.babel.js',
-            },
-        },
+        'import/resolver': 'webpack',
+        // 'import/resolver': {
+        //     webpack: {
+        //         config: 'build/webpack.config.babel.js',
+        //     },
+        // },
     }
 }
