90516680377c27bfd75b4c1a43c5688411ebebd2
Валентин Немцев <v-nemcev@yandex-team.ru>
2018-01-09 17:07:29 +0300

    Fix unused vars

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ module.exports = {
             'warn',
             {
                 varsIgnorePattern: '(^\\$Conkitty|^_$)',
+                argsIgnorePattern: '(^\\$Conkitty|^_$)',
                 args: 'after-used'
             },
         ],
